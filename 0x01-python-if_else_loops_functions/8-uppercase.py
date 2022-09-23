2d8152a2a59931eae8f99da18b3229764163054b
yibe1 <yibe1481@gmail.com>
2022-09-23 14:24:59 +0300

    check lower or upper case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     x = len(str)
     for i in range(x):
         y = ord(str[i])
-        if y < 97 and y > 121:
+        if y < 97 and y > 122:
             print(str[i],end='')
         else:
             print("{}".format(chr(ord(str[i])-32)),end='')
