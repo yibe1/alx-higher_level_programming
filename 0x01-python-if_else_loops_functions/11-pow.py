52f5871dbb213a653d09f9f1ba8d2e334941fac8
yibe1 <yibe1481@gmail.com>
2022-09-23 16:21:32 +0300

    power function

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def pow(a, b):
     x = 1
-    for i in range(b):
+    if b < 0:n = b * -1
+    else:n = b
+    for i in range(n):
         x = x * a
     if(b < 0):
         return 1 / x
