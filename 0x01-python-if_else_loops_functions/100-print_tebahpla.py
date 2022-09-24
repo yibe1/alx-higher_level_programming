f3dbb7e60ff734bd502bf0154b2a930c72364682
yibe1 <yibe1481@gmail.com>
2022-09-24 20:21:10 +0300

    ascii printing

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-j = 25;
+j = 25
 for i in range(97, 123):
-    num = 97 + j;
+    num = 97 + j
     if(num % 2 == 0):
-        ch = chr(num);
+        ch = chr(num)
     else:
-        ch = chr(num - 32);
+        ch = chr(num - 32)
     print("{}".format(ch), end='')
-    j-=1
+    j -= 1
