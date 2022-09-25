05dfabc5671d04784ed307e8733f072ef2023a12
yibe1 <yibe1481@gmail.com>
2022-09-25 18:17:42 +0300

    calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+from calculator_1 import add, sub, mul, div as tk
 
 def main():
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, tk.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, tk.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, tk.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, tk.div(a, b)))
 if __name__ == "__main__":
     main()
 
