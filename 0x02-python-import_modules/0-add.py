dff2c64a5b56047eba1d062d17c4c894784b3513
yibe1 <yibe1481@gmail.com>
2022-09-25 15:36:47 +0300

    submiting readme file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import add_0 as yibe
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, yibe.add(1,2)))
+class _add_0:
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, yibe.add(1,2)))
