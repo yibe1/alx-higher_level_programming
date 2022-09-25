0efdaf8a2b8db5198ab1deddf48f93a9788813e9
yibe1 <yibe1481@gmail.com>
2022-09-25 16:31:14 +0300

    added permission

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,4 +4,4 @@ from add_0 import add as yibe
 if __name__ == '__main__':
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, yibe.add(1,2)))
+    print("{:d} + {:d} = {:d}".format(a, b, yibe.add(1,2)))
