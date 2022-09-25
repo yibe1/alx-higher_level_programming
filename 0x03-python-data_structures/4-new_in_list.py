fef371b53444e25cf21b5822b6a30ce2ffe69085
yibe1 <yibe1481@gmail.com>
2022-09-25 22:35:11 +0300

    code to print list content in reverse order

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        return my_list
+        new_list = my_list
+        return new_list
     elif idx > len(my_list) - 1:
-        return my_list
+        new_list = my_list
+        return new_list
     if my_list:
         new_list = []
         for data in my_list:
