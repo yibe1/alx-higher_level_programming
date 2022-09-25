2908df401545ecbb935963863e3a00b90b1a638f
yibe1 <yibe1481@gmail.com>
2022-09-25 16:48:37 +0300

    added code to provent import other than main

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 from add_0 import add as yibe
 
-if __name__ == '__main__':
+def main():
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, yibe.add(1,2)))
+
+
+if __name__ == '__main__':
+        main()
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import calculator_1 as yb
+
+a = 
