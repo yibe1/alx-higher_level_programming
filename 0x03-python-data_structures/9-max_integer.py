28ce459f8dcda63b7f1488dd8acccc3840fb0bca
yibe1 <yibe1481@gmail.com>
2022-09-26 16:38:37 +0300

    code that find maximum from a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max = 0
-    if len(my_list) == 0:
-        return None
+    if(my_list):
+        if len(my_list) == 0:
+            return None
+        else:
+            max = my_list[0]
+        for i in range(len(my_list) - 1):
+            if max < my_list[i]:
+                max = my_list[i]
     else:
-        max = my_list[0]
-    for i in range(len(my_list) - 1):
-        if max < my_list[i]:
-            max = my_list[i]
+        return None
     return max
