021664b871d8d11b2c0ad6e1cabdb00479902467
yibe1 <yibe1481@gmail.com>
2022-09-26 17:11:58 +0300

    finds multiple of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,9 +4,9 @@ def divisible_by_2(my_list=[]):
         bool_list = []
         for i in range(len(my_list) - 1):
             if my_list[i] % 2 == 0:
-                bool_list[i] = True
+                bool_list.append(True)
             else:
-                bool_list[i] = False
+                bool_list.append(False)
         return bool_list
     else:
         return None
