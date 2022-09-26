5585b966e0f8387a4638e32eef80cd52c47ec307
yibe1 <yibe1481@gmail.com>
2022-09-26 16:14:51 +0300

    code to a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    if sentence != None:
+    length = 0
+    if len(sentence) > 0:
         ch = sentence[0]
+        length = len(sentence)
     else:
         ch = None
     return length, ch
