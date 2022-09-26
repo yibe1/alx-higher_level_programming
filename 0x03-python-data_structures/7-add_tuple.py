b42274982eb6c65f13a975c76d19412b76a147e4
yibe1 <yibe1481@gmail.com>
2022-09-26 15:06:30 +0300

    tupple adding

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = 0
+    b = 0
+    n11 = 0
+    n12 = 0
+    n21 = 0
+    n22 = 0
+
+    if tuple_a[0]:
+        n11 = tuple_a[0]
+    if tuple_a[1]:
+        n12 = tuple_a[1]
+    if tuple_b[0]:
+        n21 = tuple_b[0]
+    if tuple_b[1]:
+        n22 = tuple_b[1]
+    new_tuple = (n11 + n21, n12 + n22)
+    return new_tuple
