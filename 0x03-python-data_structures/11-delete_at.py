b981d3ffac5226db25b27968f170f81768a63a25
yibe1 <yibe1481@gmail.com>
2022-09-26 17:58:23 +0300

    code to delete element form a list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,10 +2,5 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
-    new_list = []
-    for i in range(len(my_list)):
-        if i == idx:
-            continue
-        else:
-            del my_list[i]
+    del my_list[idx]
     return my_list
