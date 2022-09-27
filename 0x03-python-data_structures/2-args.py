32f1f702bee49fb2241c248bec91d69f4f8d6897
yibe1 <yibe1481@gmail.com>
2022-09-27 11:50:16 +0300

    number of args

---
+++ 0x03-python-data_structures/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if "__name__" == "main":
+    size = len(argv)
+    if size == 1:
+        print("{} argument:".format(i))
+    else:
+        if size == 0:
+            print("{} arguments.".format(i))
+        else:
+            print("{} arguments:".format(i))
+    for i in range(size):
+        print("{:d:} {}".format(i + 1, argv[i]))
