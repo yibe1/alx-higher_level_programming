532c78bea721f1325a5b42ef66c1b289f18906ff
yibe1 <yibe1481@gmail.com>
2022-09-27 12:35:28 +0300

    number of args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ from sys import argv
 if __name__ == "__main__":
     size = len(argv)
     if size == 1:
-        print("{} argument:".format(i))
+        print("{} argument:".format(1))
     else:
         if size == 0:
-            print("{} arguments.".format(i))
+            print("{} arguments.".format(0))
         else:
-            print("{} arguments:".format(i))
-    for i in range(size):
-        print("{:d:} {}".format(i + 1, argv[i]))
+            print("{} arguments:".format(size))
+    for i in range(1, size):
+        print("{:d}: {}".format(i, argv[i]))
