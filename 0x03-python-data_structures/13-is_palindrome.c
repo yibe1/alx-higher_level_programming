15021bea30fc3831cfff39dcd97138851582829b
yibe1 <yibe1481@gmail.com>
2022-09-27 04:45:58 +0300

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,45 +4,49 @@
 /**
  * is_palindrome-checks if a list is palindrom
  * @head: the head of the lis
- * 
+ *
  * Return: 1 if palindrome and 0 otherwise
- * 
+ *
  */
 
 int is_palindrome(listint_t **head)
 {
     listint_t *temp = *head, *temp0 = *head;
-    struct mylist *theList=NULL, *last= NULL, *theHead= NULL;
-    while(temp != NULL)
+    struct mylist *theList = NULL, *last = NULL, *theHead = NULL;
+    while (temp != NULL)
     {
-      theList =(struct mylist*) malloc(sizeof(struct mylist));
-        theList->n = temp->n;
-        theList->next = NULL;
-        if(theHead == NULL)
-        {
-            theList->prev = NULL;
-            theHead = theList;
-            continue;
-        }
-       
-        struct mylist* hc = (struct mylist*)theHead;
-        while(hc->next != NULL)
-	  hc = (struct mylist*)hc->next;
-        hc -> next = (struct mylist*)theList;
-        theList -> prev = hc;
-        last = theList;
-         temp = temp->next;
+      theList = (struct mylist *) malloc(sizeof(struct mylist));
+      theList->n = temp->n;
+      theList->next = NULL;
+      if (theHead == NULL)
+	{
+	  theList->prev = NULL;
+	  theHead = theList;
+	  continue;
+	}
+
+      struct mylist *hc = theHead;
+      while (hc->next != NULL)
+	hc = hc->next;
+      hc->next = theList;
+      theList->prev = hc;
+      last = theList;
+      temp = temp->next;
     }
-    while(temp0 != NULL){
-        if(last->n == temp0->n){
-            last = last->prev;
-            temp0 = temp0->next;
-            continue;
-        }else{
-            return 0;
-        }
-        last = last->prev;
-        temp0 = temp0->next;
-    }
-    return 1;
+    while (temp0 != NULL)
+      {
+	if (last->n == temp0->n)
+	  {
+	    last = last->prev;
+	    temp0 = temp0->next;
+	    continue;
+	  }
+	else
+	  {
+	    return (0);
+	  }
+	last = last->prev;
+	temp0 = temp0->next;
+      }
+    return (1);
 }
