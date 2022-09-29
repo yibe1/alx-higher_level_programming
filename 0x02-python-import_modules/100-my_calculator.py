e1c5a340720ecb5010e26423fc922c15bab114b2
yibe1 <yibe1481@gmail.com>
2022-09-29 18:45:18 +0300

    updates

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,10 +2,12 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, div, mul
     from sys import argv
+
+    a = int(argv[1])
+    b = int(argv[3])
     if len(argv) != 4:
         print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
         return 1
-    a = int(argv[1]), b = int(argv[3])
     if argv[2] == "+":
         print("{:d} {} {:d} = {:d}".format(a, argv[2], b, add(a,b)))
     elif argv[2] == "-":
