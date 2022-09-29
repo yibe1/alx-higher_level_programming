74e281a2931dd7b51199f0e2e5af237827360761
yibe1 <yibe1481@gmail.com>
2022-09-29 18:55:16 +0300

    updates

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         print("{:d} {} {:d} = {:d}".format(a, argv[2], b, add(a,b)))
     elif argv[2] == "-":
         print("{:d} {} {:d} = {:d}".format(a, argv[2], b, sub(a,b)))
-    elif argv[3] == "*":
+    elif argv[2] == "*":
         print("{:d} {} {:d} = {:d}".format(a, argv[2], b, mul(a,b)))
     elif argv[2] == "/":
         print("{:d} {} {:d} = {:d}".format(a, argv[2], b, div(a,b)))
